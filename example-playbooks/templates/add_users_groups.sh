9a9be9b423f1800f632c93038e544a2e986ccabf
Ryo Nakamura <upa@haeena.net>
2021-09-20 21:09:55 +0900

    save

---
+++ example-playbooks/templates/add_users_groups.sh
@@ -0,0 +1,12 @@
+# add primary group of all users
+for x in {{ ldap_users.list | map(attribute="gid") | join(" ") }} ; do
+  if ! ldapsearch -x -D cn=admin,{{ ldap_domain }} -b cn=${x},ou=groups,{{ ldap_domain }} -w {{ ldap_root_password }} > /dev/null ; then
+    cat  /tmp/${x}.ldif ;
+  fi ;
+done | ldapadd -x -D cn=admin,{{ ldap_domain }} -w {{ ldap_root_password }}
+# add all users
+for x in {{ ldap_users.list | map(attribute="uid") | join(" ") }} ; do
+  if ! ldapsearch -x -D cn=admin,{{ ldap_domain }} -b uid=${x},ou=people,{{ ldap_domain }} -w {{ ldap_root_password }} > /dev/null ; then
+    cat  /tmp/${x}.ldif ;
+  fi ;
+done | ldapadd -x -D cn=admin,{{ ldap_domain }} -w {{ ldap_root_password }}
